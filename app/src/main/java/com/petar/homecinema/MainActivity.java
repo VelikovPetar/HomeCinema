e3b2faf3ff0b2f612f75bb40ae50b283a01bd1eb
VelikovPetar <petarvelikov95@gmail.com>
2016-06-10 12:23:04 +0200

    Added classes to handle pre-populated database

--- app/src/main/java/com/petar/homecinema/MainActivity.java
+++ app/src/main/java/com/petar/homecinema/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
 
     DatabaseAccess da;
