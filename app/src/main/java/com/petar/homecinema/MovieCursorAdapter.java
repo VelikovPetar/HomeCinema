6c6881e708df72ad06af4ba5a938d9aa5c862824
VelikovPetar <petarvelikov95@gmail.com>
2017-01-15 17:34:41 +0100

    MainActivity appearance

--- app/src/main/java/com/petar/homecinema/MovieCursorAdapter.java
+++ app/src/main/java/com/petar/homecinema/MovieCursorAdapter.java
@@ -2,7 +2,6 @@ package com.petar.homecinema;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
